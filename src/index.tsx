e031c2e37ef0d067698b02bd3e1df795f06e6e70
realcaldeira <mrrealcaldeira@gmail.com>
2021-11-05 16:34:48 -0300

    Listando transactions e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,34 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-12 11:00:00'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      this.schema.all('transaction')
+      return this.schema.all('transaction')
     })
 
     this.post('/transactions', (schema, request) => {
